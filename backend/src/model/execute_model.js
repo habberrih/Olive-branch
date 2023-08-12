3105751d031e183369ef4ccbc959c0bc196f4956
12a02d <abdul2013ly@gmail.com>
2023-08-12 08:29:40 +0300

    feat: link python with nodejs

---
+++ backend/src/model/execute_model.js
@@ -0,0 +1,46 @@
+const { spawn } = require('node:child_process');
+
+const fs = require('fs');
+
+function writeStringToFile(filePath, content) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Successfully wrote to file:', filePath);
+    }
+  });
+}
+
+function executeOliveModel(image_path) {
+  return new Promise((resolve, reject) => {
+    const python = spawn('python', [
+      'G:/vscode/oliver-trees/backend/src/model/model.py',
+      'G:/vscode/oliver-trees/A299.jpg', // image path
+    ]);
+
+    let resultData = '';
+
+    python.stdout.on('data', (resultFromPython) => {
+      console.log('Python: Pipe data from python script ...');
+      const data = resultFromPython.toString();
+      resultData += data.toString();
+    });
+
+    python.on('close', (code) => {
+      console.log(`Node: executed python successfully with code: ${code}`);
+      console.log('Result from Python:', resultData);
+      writeStringToFile('result.txt', resultData);
+      resolve();
+    });
+
+    python.on('error', (err) => {
+      console.error('Failed to execute Python script:', err);
+      reject(err);
+    });
+  });
+}
+
+module.exports = {
+  executeOliveModel,
+};
