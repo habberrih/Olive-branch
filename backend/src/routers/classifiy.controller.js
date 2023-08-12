97f87a70f6180449e5de1424c585bc79f090915c
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-08-12 13:35:17 +0200

    refactor: read uploaded img from file

--- backend/src/routers/classifiy.controller.js
+++ backend/src/routers/classifiy.controller.js
@@ -1,22 +1,29 @@
-const { executeOliveModel } = require('../model/execute_model');
-const fs = require('fs');
-const path = require('path');
+const { executeOliveModel } = require("../model/execute_model");
+const fs = require("fs");
+const path = require("path");
 
 async function httpGetModelResult(req, res) {
   try {
-    const image = 'G:/vscode/oliver-trees/A299.jpg';
-    await executeOliveModel(image);
-    const modelResultPath = path.join(__dirname, '..', '..', 'result.txt');
-    fs.readFile(modelResultPath, 'utf8', (err, data) => {
+    const file = req.file;
+    const image_path = path.join(
+      __dirname,
+      "..",
+      "model",
+      "uploaded_images",
+      `${file.originalname}`
+    );
+    await executeOliveModel(image_path);
+    const modelResultPath = path.join(__dirname, "..", "..", "result.txt");
+    fs.readFile(modelResultPath, "utf8", (err, data) => {
       if (err) {
-        console.error('Error reading file:', err);
+        console.error("Error reading file:", err);
       } else {
         return res.status(200).json(data);
       }
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json('Internal Server Error');
+    return res.status(500).json("Internal Server Error");
   }
 }
 
